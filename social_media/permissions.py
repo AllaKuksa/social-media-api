69117195a68d8316004e73ea205c58a0938eb069
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-14 20:13:32 +0300

    Added IsAdminOrIsAuthenticated permission for ProfileViewSet

---
+++ social_media/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+
+
+class IsAdminOrIsAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method is SAFE_METHODS and
+            request.user and
+            request.user.is_authenticated) or (
+            request.user and request.user.is_staff
+        )
--- social_media/views.py
+++ social_media/views.py
@@ -1,12 +1,14 @@
 from rest_framework import viewsets
 
 from social_media.models import Profile
+from social_media.permissions import IsAdminOrIsAuthenticated
 from social_media.serializers import ProfileSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [IsAdminOrIsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
