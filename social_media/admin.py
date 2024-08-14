a4c9c740b86b9ad64cd8d84a406e01117cddb318
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-14 19:18:35 +0300

    Register models in admin panel

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import (
+    Post,
+    Comment,
+    Like,
+    Follow,
+    Profile
+)
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follow)
+admin.site.register(Profile)
