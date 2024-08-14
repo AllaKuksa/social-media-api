dd0ba8aa0244744b19ce6487bc344c3c0d64f653
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-14 19:19:18 +0300

    Added validate_follow method in Follow model

--- social_media/models.py
+++ social_media/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
 from phonenumber_field.modelfields import PhoneNumberField
+from rest_framework.exceptions import ValidationError
 
 
 def profile_image_file_path(instance, filename):
@@ -25,20 +26,24 @@ class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profiles"
     )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     biography = models.TextField()
-    profile_picture = models.ImageField(null=True, upload_to=profile_image_file_path)
-    phone_number = PhoneNumberField(null=True, blank=True, unique=True)
+    profile_picture = models.ImageField(
+        null=True, blank=True, upload_to=profile_image_file_path
+    )
+    phone_number = PhoneNumberField(unique=True)
     birth_date = models.DateField(null=True, blank=True)
 
     @property
     def full_name(self):
-        return f"{self.user.first_name} {self.user.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
         return self.full_name
 
     class Meta:
-        ordering = ["full_name"]
+        ordering = ["first_name", "last_name"]
         verbose_name_plural = "profiles"
 
 
@@ -53,6 +58,19 @@ class Follow(models.Model):
 
     class Meta:
         ordering = ["created_at"]
+        unique_together = ["follower", "following"]
+
+    @staticmethod
+    def validate_follow(follower, following, error_to_raise):
+        if follower == following:
+            raise error_to_raise("You can't follow yourself")
+
+    def clean(self):
+        Follow.validate_follow(self.following, self.follower, ValidationError)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(Follow, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.follower} follows {self.following}"
@@ -71,14 +89,14 @@ class Post(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="posts")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    media = models.ImageField(null=True, upload_to=profile_media_file_path)
+    media = models.ImageField(null=True, blank=True, upload_to=profile_media_file_path)
     hashtag = models.CharField(max_length=50, choices=HashtagChoices.choices)
 
     class Meta:
         ordering = ["-created_at"]
 
     def __str__(self):
-        return f"Post by {self.author} with content: {self.content}"
+        return f"Post by {self.author}: {self.content}"
 
 
 class Comment(models.Model):
@@ -102,7 +120,7 @@ class Like(models.Model):
     liked_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ["profile", "post"]
+        unique_together = ["author", "post"]
         ordering = ["-liked_at"]
 
     def __str__(self):
