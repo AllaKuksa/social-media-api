5b7a105706236116a823ae3c8985ba5eecb6371b
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-14 18:11:33 +0300

    Added Post model

--- social_media/models.py
+++ social_media/models.py
@@ -14,6 +14,13 @@ def profile_image_file_path(instance, filename):
     return os.path.join("uploads/profiles/", filename)
 
 
+def profile_media_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/media/", filename)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
@@ -51,4 +58,28 @@ class Follow(models.Model):
     def __str__(self):
         return f"{self.follower} follows {self.following}"
 
+
+class Post(models.Model):
+
+    class HashtagChoices(models.TextChoices):
+        LOVE = "Love"
+        LIFE = "Life"
+        INSPIRATION = "Inspiration"
+        TRAVEL = "Travel"
+        FITNESS = "Fitness"
+        FOOD = "Food"
+
+    author = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="posts"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    media = models.ImageField(null=True, upload_to=profile_media_file_path)
+    hashtag = models.CharField(max_length=50, choices=HashtagChoices.choices)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return self.content
 
