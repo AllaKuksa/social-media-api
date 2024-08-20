83decd65fc4a455b3250c406e7f856b483c9e722
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-20 19:46:13 +0300

    Added default pagination for all project

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -144,6 +144,8 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 5
 }
 
 SPECTACULAR_SETTINGS = {
