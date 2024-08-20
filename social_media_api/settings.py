f98fe4d6ecb1e83b4e2680a010fdc3a9355d1d54
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-20 19:48:36 +0300

    Added default throttle classes

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -145,7 +145,12 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 5
+    "PAGE_SIZE": 5,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 SPECTACULAR_SETTINGS = {
